83a282a051b127694b277362a7fc48ea8d51c0c9
Lukasz Patalas <lpatalas@gmail.com>
2023-03-13 21:44:29 +0100

    Show all errors at once in build script instead of failing after the first one

--- scripts/BuildRelease.ps1
+++ scripts/BuildRelease.ps1
@@ -5,6 +5,13 @@ param()
 Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
 
+$script:HasErrors = $false
+
+function LogError($message) {
+    $script:HasErrors = $true
+    Write-Error -Message $message -ErrorAction Continue
+}
+
 $rootPath = Split-Path $PSScriptRoot
 $artifactsPath = Join-Path $rootPath artifacts
 $publishPath = Join-Path $artifactsPath publish
@@ -35,16 +42,16 @@ if (-not $expectedVersion) {
 }
 
 if ($expectedVersion -ne $manifestInfo.Version) {
-    throw "Module version in psd1 ($($manifestInfo.Version)) does not match Version.props ($expectedVersion)"
+    LogError "Module version in psd1 ($($manifestInfo.Version)) does not match Version.props ($expectedVersion)"
 }
 
 $expectedLicenseUrl = "$($manifestInfo.ProjectUri)/blob/v$expectedVersion/LICENSE.txt"
 if ($manifestInfo.LicenseUri -ne $expectedLicenseUrl) {
-    throw "License URI '$($manifestInfo.LicenseUri)' does not match expected value '$expectedLicenseUrl'"
+    LogError "License URI '$($manifestInfo.LicenseUri)' does not match expected value '$expectedLicenseUrl'"
 }
 
 if ($manifestInfo.ReleaseNotes -notlike "v$expectedVersion*") {
-    throw "Release notes should be updated in psd1 file"
+    LogError "Release notes should be updated in psd1 file"
 }
 
 $helpSourcePath = Join-Path $rootPath 'help'
@@ -53,8 +60,12 @@ Get-ChildItem $helpSourcePath `
     $onlineVersion = (Select-String -Path $_ -Pattern '^online version:').Line
     $expectedHelpUri = "$($manifestInfo.ProjectUri)/blob/v$expectedVersion/help/$($_.Name)"
     if ($onlineVersion -ne "online version: $expectedHelpUri") {
-        throw "Online URL in '$_' does not match the current version and/or file name '$onlineVersion'. Expected: '$expectedHelpUri'"
+        LogError "Online URL in '$_' does not match the current version and/or file name '$onlineVersion'. Expected: '$expectedHelpUri'"
     }
+}
+
+if ($script:HasErrors) {
+    throw "Failing script because of errors"
 }
 
 New-ExternalHelp -Path $helpSourcePath -OutputPath $modulePath | Out-Null
